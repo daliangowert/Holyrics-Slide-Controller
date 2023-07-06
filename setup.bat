aebf170427d161e2946fda4d0484e23204a1f0b1
daliangowert <dalian_gowert@hotmail.com>
2023-07-05 20:31:06 -0400

    Alterações setup

--- setup.bat
+++ setup.bat
@@ -9,29 +9,21 @@ if %errorlevel% equ 0 (
     winget install -e --id OpenJS.NodeJS
 )
 
-REM Verificar se o Python 3.10 está instalado
-python -c "import sys; sys.exit(sys.version_info < (3, 10))"
-if %errorlevel% equ 0 (
-    echo Python 3.10 esta instalado.
-) else (
-    echo Python 3.10 nao esta instalado. Instalando...
-    winget install -e --id Python.Python.3.10
-)
-
 @REM Instalar dependências Node
-npm ci
+echo Instalando dependências Node...
+call npm ci
 
 @REM Instalar node-windows
-npm install -g node-windows
-npm link node-windows
+call npm install -g node-windows
+call npm link node-windows
 
 REM Verifica se o serviço existe
 set SERVICE_NAME=Holyrics_Controller
 sc query %SERVICE_NAME% > nul 2>&1
 if %errorlevel% equ 0 (
-    echo O serviço %SERVICE_NAME% ja existe.
+    echo O servico %SERVICE_NAME% ja existe.
 ) else (
-    echo O serviço %SERVICE_NAME% nao existe. Executando o comando 'node createService'...
+    echo O servico %SERVICE_NAME% nao existe. Executando o comando 'node createService'...
     node createService
 )
 echo Serviço p/ Windows criado!
